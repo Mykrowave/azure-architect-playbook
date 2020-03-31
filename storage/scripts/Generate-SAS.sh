8408c8663e8104bf2d903e000377df80e8cd271f
Mykrowave <M88Parker@gmail.com>
2020-03-30 22:52:00 -0400

    Create Generate-SAS.sh

---
+++ storage/scripts/Generate-SAS.sh
@@ -0,0 +1,25 @@
+# Assign Role for generateUserDelegationKey
+az role assignment create \
+    --role "Storage Blob Data Contributor" \
+    --assignee <email> \
+    --scope "/subscriptions/<subscription>/resourceGroups/<resource-group>/providers/Microsoft.Storage/storageAccounts/<storage-account>"
+
+# Generate SAS for CONTAINER
+az storage container generate-sas \
+    --account-name <storage-account> \
+    --name <container> \
+    --permissions acdlrw \
+    --expiry <date-time> \
+    --auth-mode login \
+    --as-user
+
+# Generate SAS for a BLOB  
+az storage blob generate-sas \
+    --account-name <storage-account> \
+    --container-name <container> \
+    --name <blob> \
+    --permissions acdrw \
+    --expiry <date-time> \
+    --auth-mode login \
+    --as-user
+    --full-uri
