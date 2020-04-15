3547e3761480f044c5e3b4e95647cea806c6a469
Mykrowave <M88Parker@gmail.com>
2020-04-14 21:45:11 -0400

    Update docker-commands.sh

--- containerization/scripts/docker-commands.sh
+++ containerization/scripts/docker-commands.sh
@@ -12,3 +12,26 @@ docker run mcr.microsoft.com/dotnet/core/samples:aspnetapp
 # -d to let docker to run in background (it isnt meant to be used interactively)
 docker run -p 8080:80 -d mcr.microsoft.com/dotnet/core/samples:aspnetapp
 
+# view active containers
+docker containers ls
+
+# shortcut to view active containers
+docker ps
+
+# stop container
+docker stop <container-id>
+
+# view all containers (Even stopped)
+docker ps -a
+
+# restart a container
+docker start <container-id>
+
+# remove a container
+docker rm <container-id>
+
+# remove a docker image
+docker image rm mcr.microsoft.com/dotnet/core/samples:aspnetapp
+
+
+
