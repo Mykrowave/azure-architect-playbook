f56f2024edbf1e2804924287083db6500c942f19
Mykrowave <M88Parker@gmail.com>
2020-04-15 21:57:26 -0400

    Update create-container-instance.sh

--- containerization/scripts/create-container-instance.sh
+++ containerization/scripts/create-container-instance.sh
@@ -18,4 +18,26 @@ az container show \
 az container logs \
   --resource-group $rg \
   --name mycontainer-restart-demo
+  
+# Create Container with Environment Variables
+az container create \
+  --resource-group learn-deploy-aci-rg \
+  --name aci-demo \
+  --image microsoft/azure-vote-front:cosmosdb \
+  --ip-address Public \
+  --location eastus \
+  --environment-variables \
+    COSMOS_DB_ENDPOINT=$COSMOS_DB_ENDPOINT \
+    COSMOS_DB_MASTERKEY=$COSMOS_DB_MASTERKEY
+    
+ # Create Container with Secure Environment VAriables
+ az container create \
+  --resource-group learn-deploy-aci-rg \
+  --name aci-demo-secure \
+  --image microsoft/azure-vote-front:cosmosdb \
+  --ip-address Public \
+  --location eastus \
+  --secure-environment-variables \
+    COSMOS_DB_ENDPOINT=$COSMOS_DB_ENDPOINT \
+    COSMOS_DB_MASTERKEY=$COSMOS_DB_MASTERKEY
 
