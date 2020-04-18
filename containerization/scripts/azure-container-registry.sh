ef4b110ebb56d80fa10d3992ef5bfb290227ae8c
Mykrowave <M88Parker@gmail.com>
2020-04-18 10:59:24 -0400

    Update azure-container-registry.sh

--- containerization/scripts/azure-container-registry.sh
+++ containerization/scripts/azure-container-registry.sh
@@ -14,3 +14,13 @@ az acr replication list --registry $ACR_NAME --output table
 
 # # use cli to upload Docker file and other files that make up your image, then build
 az acr build --file Dockerfile --registry myregistry --image myimage .
+
+# create an ACR Task to build image every time source code is updated (github version)
+az acr task create \  
+--registry <container_registry_name> \  
+--name buildwebapp \  
+--image webimage \  
+--context https://github.com/MicrosoftDocs/mslearn-deploy-run-container-app-service.git \  
+--branch master \  
+--file Dockerfile \  
+--git-access-token <access_token>
