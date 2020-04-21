82899a17082408b5e50c62883ee61da84ba9d726
Mykrowave <M88Parker@gmail.com>
2020-04-21 19:52:12 -0400

    Update vm-config.sh

--- vm/scripts/vm-config.sh
+++ vm/scripts/vm-config.sh
@@ -7,8 +7,8 @@ az vm create \
   --admin-username azureuser \
   --generate-ssh-keys \
   --verbose
-  
-  
+
+
 # Don't wait in console
 az vm create \
   --resource-group $rg \
@@ -19,19 +19,45 @@ az vm create \
   --generate-ssh-keys \
   --verbose
   --no-wait
-  
-  # connect with ssh
-  ssh azureuser@<public-ip-address>
-  
-  
-  # List most popular VM images
-  az vm image list --output table
-  
-  # List all and filter by sku
-  az vm image list --sku Wordpress --output table --all
-  az vm image list --publisher Microsoft --output table --all
-  
-  # List images by location
-  az vm image list --location eastus --output table
-  
-  
+
+# connect with ssh
+ssh azureuser@<public-ip-address>
+
+
+# List most popular VM images
+az vm image list --output table
+
+# List all and filter by sku
+az vm image list --sku Wordpress --output table --all
+az vm image list --publisher Microsoft --output table --all
+
+# List images by location
+az vm image list --location eastus --output table
+
+# Specify a size
+az vm create \
+    --resource-group $rg \
+    --name SampleVM2 \
+    --image UbuntuLTS \
+    --admin-username azureuser \
+    --generate-ssh-keys \
+    --verbose \
+    --size "Standard_DS5_v2"
+    
+# Get Resize Options
+az vm list-vm-resize-options \
+    --resource-group $rg \
+    --name SampleVM \
+    --output table
+    
+# Resize
+az vm resize \
+    --resource-group $rg \
+    --name SampleVM \
+    --size Standard_D2s_v3
+    
+# Get Ip Address
+az vm list-ip-addresses -n SampleVM -o table
+
+
+    
