1b113eb52f29805673f9463c972dacaa38a8ae09
Mykrowave <M88Parker@gmail.com>
2020-05-07 21:21:20 -0400

    Update disk-encryption.sh

--- vm/scripts/disk-encryption.sh
+++ vm/scripts/disk-encryption.sh
@@ -4,3 +4,17 @@ az keyvault create \
     --resource-group <resource-group> \
     --location <location> \
     --enabled-for-disk-encryption True
+    
+# Enable Encryption
+az vm encryption enable \
+    --resource-group <resource-group> \
+    --name <vm-name> \
+    --disk-encryption-keyvault <keyvault-name> \
+    --volume-type [all | os | data] \
+    
+    
+# Check if Encrypted
+az vm encryption show --resource-group <resource-group> --name <vm-name>
+
+# Reverse Encryption setting
+az vm encryption disable --resource-group <resource-group> --name <vm-name>
